d2c299b248032aa3a47c4c3fddbec071e70e201b
Austin Asbury <acasbury25@aol.com>
2019-09-01 15:04:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,4 +18,9 @@ class Artist
   def new_song (song_name, song_genre)
     Song.new(song_name, self, song_genre)
   end
+
+  def genres
+    Song.all.select {|s| s.genre if s.artist == self}
+  end
+
 end
